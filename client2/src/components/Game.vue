19404f6cdb7a66eb793abc2a4e8a9dd96c778c9c
Csongor Zih <daemon.onwave@gmail.com>
2023-04-28 21:33:48 +0200

    Implement client core concepts

--- client2/src/components/Game.vue
+++ client2/src/components/Game.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import { onBeforeUnmount, reactive, shallowRef, type Ref, getCurrentInstance, ref } from "vue";
+import { computed, onBeforeUnmount, reactive, ref, shallowRef, type Ref } from "vue";
 import type { State } from "../../../server/shared-schema";
 import { Msg, Response } from "../../../shared-enums.ts";
 import { connect } from "../connect.ts";
@@ -8,10 +8,17 @@ import HandView from "./HandView.vue";
 
 export type Card = { id: number, text: string };
 
-const hand = reactive(new Set<Card>());
 const stateHolder: Ref<State | null> = shallowRef(null);
-let winnerIndex: number | undefined = undefined;
+const hand = reactive(new Set<Card>());
+let winnerIndex: Ref<number | undefined> = ref(undefined);
+let myId: Ref<string | undefined> = ref(undefined);
 let updateKey = ref(0);
+
+const iAmCzar = computed(() => {
+	updateKey.value; // force update
+	if (stateHolder.value == null || myId.value == undefined) return false;
+	return stateHolder.value.playerStatus.get(myId.value) === "czar";
+});
 
 function resolveCard(id: number): Card {
 	// placeholder
@@ -19,6 +26,7 @@ function resolveCard(id: number): Card {
 }
 
 const room = await connect();
+myId.value = room.sessionId;
 room.send(Response.name, { text: "Username" });
 
 room.onStateChange(state => {
@@ -39,8 +47,12 @@ room.onMessage(Msg.GiveCard,
 );
 
 room.onMessage(Msg.Winner,
-	({ cardIndex }: { cardIndex: number }) => winnerIndex = cardIndex
+	({ cardIndex }: { cardIndex: number }) => winnerIndex.value = cardIndex
 );
+
+room.onMessage(Msg.Error, ({ message }: { message: string }) => {
+	console.error(message);
+});
 
 function startGame() {
 	room.send(Response.startGame);
@@ -51,6 +63,10 @@ function playCard(cards: Card[]) {
 	for (const card of cards) hand.delete(card);
 }
 
+function pickCard(index: number) {
+	room.send(Response.pickCard, { cardIndex: index });
+}
+
 onBeforeUnmount(() => room.leave());
 
 </script>
@@ -59,12 +75,82 @@ onBeforeUnmount(() => room.leave());
 	<div :key="updateKey">
 		<ul v-if="stateHolder != null">
 			<li v-for="player in stateHolder?.playerList">
-				{{ player }} :: {{ stateHolder?.playerNames?.get(player) }}
+				{{ player }}
+				:: {{ stateHolder?.playerNames?.get(player) }}
+				:: {{ stateHolder?.playerStatus?.get(player) }}
 			</li>
 		</ul>
+
+		<div id="tabletop" class="cardrow">
+			<div v-if="stateHolder?.call" class="black card">
+				<p>
+					{{ stateHolder.call.cardid.at(0) }}
+				</p>
+			</div>
+			<div v-if="stateHolder?.responses?.length">
+				<div v-for="(response, index) in stateHolder?.responses" class="white card" @click="pickCard(index)">
+					<p>
+						{{ response.cardid.join(", ") }}
+					</p>
+				</div>
+			</div>
+		</div>
+
 		<button @click="startGame">Start</button>
+		<div v-if="iAmCzar">You are the Card Czar.</div>
 		<HandView :hand="hand" :hand-size="7" @play="playCard"></HandView>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.card {
+	width: 120px;
+	height: 160px;
+	background-color: white;
+	border-radius: 10px;
+	margin: 8px;
+	padding: 12px;
+}
+
+.black {
+	background-color: black;
+	color: white;
+}
+
+.white.card {
+	width: 160px;
+}
+
+.card.winner {
+	border: rgb(0, 140, 255) 3px solid;
+	transition: transform 0.2s;
+	box-shadow: 0px 12px 29px -16px rgba(0,0,0,0.75);
+	transform: scale(1.2);
+	z-index: 3;
+}
+
+a.card {
+	background: none;
+	text-align: center;
+	color: white;
+}
+
+a.card p {
+	font-weight: bold;
+	border: white 3px solid;
+	border-radius: 10px;
+	padding: 30px 0;
+}
+
+.cardrow {
+	display: flex;
+	flex-direction: row;
+	/* Bad for accessibility, good for looks */
+	outline: none;
+	flex-wrap: wrap;
+	justify-content: center;
+}
+
+
+</style>
